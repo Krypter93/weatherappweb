5e181f5da8722bf38932b751d2ec2797303f88f9
Ahmed <ahmedfdez93@gmail.com>
2024-03-28 19:37:35 -0400

    app updated

--- weather.js
+++ weather.js
@@ -1,38 +1,39 @@
-//Grab data 
-let button = document.querySelector("button");
-let forecast = document.querySelector(".forecast");
-let country = document.querySelector(".country");
-let farenheit = document.querySelector(".farenheit");
-let condition = document.querySelector(".condition");
-let humidity = document.querySelector(".humidity");
-let wind = document.querySelector(".wind");
+//Grab data
+const button = document.querySelector("button");
+const forecast = document.querySelector(".forecast");
+const country = document.querySelector(".country");
+const farenheit = document.querySelector(".farenheit");
+const condition = document.querySelector(".condition");
+const humidity = document.querySelector(".humidity");
+const wind = document.querySelector(".wind");
+const data_wrap = document.querySelector(".data");
 
 //Fetching function
-function data(resp){
-    fetch(`https://api.weatherapi.com/v1/current.json?key=48b6a4da649f4320b2e33045232312&q=${resp}`)
-    .then(response => response.json())
-    .then(data => {
-        forecast.textContent = `Current Temperature in Celsius: ${data.current.temp_c} ºC`;
-        farenheit.textContent = `Current Temperature in Celsius: ${data.current.temp_f} ºF`;
-        country.textContent = `Country: ${data.location.country}`;
-        condition.textContent = `Condition: ${data.current.condition.text}`;
-        humidity.textContent = `Humidity: ${data.current.humidity}`;
-        wind.textContent = `Wind: ${data.current.wind_mph}`;
-    })
-    }
+function data(resp) {
+  fetch(
+    `https://api.weatherapi.com/v1/current.json?key=48b6a4da649f4320b2e33045232312&q=${resp}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      forecast.textContent = `Current Temperature in Celsius: ${data.current.temp_c} ºC`;
+      farenheit.textContent = `Current Temperature in Celsius: ${data.current.temp_f} ºF`;
+      country.textContent = `Country: ${data.location.country}`;
+      condition.textContent = `Condition: ${data.current.condition.text}`;
+      humidity.textContent = `Humidity: ${data.current.humidity}`;
+      wind.textContent = `Wind: ${data.current.wind_mph}`;
+    });
+}
 
 button.addEventListener("click", () => {
-    let input = document.querySelector("#input").value;
+  let input = document.querySelector("#input").value;
 
-        if(!input){
-            alert("Missing data");
-        }
-        
-        if(input){
-            console.log(input);
-            console.log(typeof input);
-        }
+  if (!input) {
+    alert("Missing data");
+  }
 
-        data(input)
-    })
-    
+  data(input);
+
+  document.querySelector("#input").value = "";
+
+  data_wrap.classList.add("active");
+});
